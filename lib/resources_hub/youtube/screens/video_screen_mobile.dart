d6561f0dbb147a32acd5a0f5487d260f0fc7e3d4
Harsshad <harsshadsivsharan@gmail.com>
2025-05-11 01:53:48 +0530

    made certain changes to the youtube section of resource hub and now its working on both web and mobile, made YouTube API work on web using youtube_iframe_id and on mobile using youtube_player_flutter

--- lib/resources_hub/youtube/screens/video_screen_mobile.dart
+++ lib/resources_hub/youtube/screens/video_screen_mobile.dart
@@ -1,7 +1,5 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoScreen extends StatefulWidget {
